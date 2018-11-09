533736fc28197e62e8a93815d1ca5cbb5a263ec6
Chionia Kotsalidou <chionia.kotsalidou@student.manchester.ac.uk>
2018-11-09 17:24:26 +0000

    Stub for the Lon Jatham NPC class.
    This stub should follow the HolidayWomanNPC class. Also Lon Jatham
    should be added to the list of NPCs.

---
+++ src/games/stendhal/server/maps/ados/city/LonJathamNPC.java
@@ -0,0 +1,23 @@
+package games.stendhal.server.maps.ados.city;
+
+import java.util.Map;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+
+public class LonJathamNPC implements ZoneConfigurator {
+
+	
+	@Override
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public static boolean isTalking() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
